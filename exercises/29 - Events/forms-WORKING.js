589f0610f89baa4e3f0a94d7b8ef4b895cbaf1f9
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-04-22 19:02:04 -0700

    Beginning JavaScript
    - Events and Etch a Sketch exercises

--- exercises/29 - Events/forms-WORKING.js
+++ exercises/29 - Events/forms-WORKING.js
@@ -12,40 +12,106 @@ jeremy.addEventListener('click', function(event) {
 });
 
 const signupForm = document.querySelector('[name="signup"]');
-signupForm.addEventListener('submit', function(event) {
-  // console.log(event);
-  const name = event.currentTarget.name.value;
+const formId = document.querySelector('#signup');
+const namebox = document.querySelector('#name');
+const emailbox = document.querySelector('#email');
+const submitButton = document.querySelector('#button-submit');
 
-  if (name.includes('chad')) {
-    console.log('Sorry Bro');
-    event.preventDefault();
-  }
+function validateEmail(event) {
+  console.log(event);
+
   // event.preventDefault();
   console.log(event.currentTarget.name.value);
   console.log(event.currentTarget.email.value);
   console.log(event.currentTarget.agree.value);
-});
+
+  // const name = event.currentTarget.name.value;
+  // if (name.includes('chad')) {
+  //   alert('Sorry Bro');
+  //   event.preventDefault();
+  // }
+
+  const ui = {
+    formId,
+    namebox,
+    emailbox,
+    submit: submitButton,
+  };
+  console.log(`ui Object:`, ui);
+
+  // regex for valid email
+  const regexEmail = new RegExp(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+  );
+
+  let validEmail;
+
+  if (ui.formId) {
+    console.log(`.:: DEBUG ::. inside the ui.formId check`);
+    // remove any pre-existing error class
+    if (ui.formId !== undefined) {
+      ui.formId.classList.remove('has-error');
+    }
+    if (ui.emailbox !== undefined) {
+      ui.emailbox.classList.remove('has-error');
+    }
+
+    ui.formId.submit(e => {
+      console.log(`.:: DEBUG ::. inside the ui.formId.submit check`);
+      e.preventDefault();
+
+      // validation code
+      validEmail = regexEmail.test(ui.emailbox.value);
+
+      console.log(`validEmail:`, validEmail);
+
+      if (!validEmail) {
+        ui.emailbox.classList.add('has-error');
+        console.log(`.:: DEBUG ::. Not a valid email format`);
+      } else {
+        console.log(`.:: DEBUG ::. Successfully submitted the form!`);
+        console.log(`.:: DEBUG ::. Here is the POST'ed form data`, ui.formId.serialize());
+      }
+    });
+  }
+}
+
+formId.addEventListener('submit', validateEmail);
+// signupForm.addEventListener('submit', function(event) {
+//   console.log(event);
+
+//   // event.preventDefault();
+//   console.log(event.currentTarget.name.value);
+//   console.log(event.currentTarget.email.value);
+//   console.log(event.currentTarget.agree.value);
+
+//   if (name.includes('chad')) {
+//     console.log('Sorry Bro');
+//     event.preventDefault();
+//   }
+
+// });
 
 function logEvent(event) {
   console.log(event.type);
 }
 
-signupForm.name.addEventListener('keyup', logEvent);
-signupForm.name.addEventListener('keydown', logEvent);
-signupForm.name.addEventListener('focus', logEvent);
-signupForm.name.addEventListener('blur', logEvent);
+// signupForm.name.addEventListener('keyup', logEvent);
+// signupForm.name.addEventListener('keydown', logEvent);
+// signupForm.name.addEventListener('focus', logEvent);
+// signupForm.name.addEventListener('blur', logEvent);
 
 // 'keyup'
 // 'keydown'
 // 'focus'
 // 'blur'
 
-const photo = document.querySelector('.photo');
+// const photo = document.querySelector('.photo');
 
-function handlePhotoClick(event) {
-  if (event.type === 'click' || event.type === 'Enter') {
-    console.log('You clicked the photo');
-  }
-}
-photo.addEventListener('click', handlePhotoClick);
-photo.addEventListener('keyup', handlePhotoClick);
+// function handlePhotoClick(event) {
+//   if (event.type === 'click' || event.type === 'Enter') {
+//     console.log('You clicked the photo');
+//   }
+// }
+// photo.addEventListener('click', handlePhotoClick);
+// photo.addEventListener('keyup', handlePhotoClick);
---
+++ exercises/33 - Etch-a-Sketch/assets/etch-a-sketch-WORKING.js
@@ -0,0 +1,89 @@
+/* eslint-disable */
+// Select the elements on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 50;
+// Setup our canvas for drawing
+// make a variable called height and width from the same properties on our canvas.
+const { width, height } = canvas;
+
+// write a function to update the brushSize
+const adjustBrushSize = e => {
+  // capture brushsize
+  // set the lineWidth to the text field value
+  // set the MOVE_AMOUNT to the text field value
+  MOVE_AMOUNT = e.target.value;
+  console.log(`Brush size updated to ${MOVE_AMOUNT}`);
+};
+
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+// create random x and y starting points on the canvas
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+ctx.beginPath(); // start the drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
+// write a draw function
+function draw({ key }) {
+  // increment the hue
+  hue += 10;
+  console.log(hue);
+  ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  console.log(key);
+  // start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // move our x and y values depending on what the user did
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+// write a handler for the keys
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
+// clear /shke function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    function() {
+      console.log('Done the shake!');
+      canvas.classList.remove('shake');
+    },
+    { once: true }
+  );
+}
+
+// listen for arrow keys
+window.addEventListener('keydown', handleKey);
+shakebutton.addEventListener('click', clearCanvas);
